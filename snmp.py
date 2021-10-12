09de407f381e0a34c00555d9c0f256ce87a5b479
dawix10 <dawid.pustowka@gmail.com>
2021-10-12 12:05:02 +0200

    s

--- snmp.py
+++ snmp.py
@@ -28,4 +28,31 @@ def get(fname):
             print(y[-1])
     pass
 
-get("172.16.0.2")
+def walk(host, oid):
+
+    for (errorIndication,
+         errorStatus,
+         errorIndex,
+         varBinds) in nextCmd(SnmpEngine(),
+                              CommunityData('public'),
+                              UdpTransportTarget((host, 161)),
+                              ContextData(),
+                              ObjectType(ObjectIdentity(oid)),
+                              lookupMib=False,
+                              lexicographicMode=False):
+
+        if errorIndication:
+            print(errorIndication, file=sys.stderr)
+            break
+
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'), file=sys.stderr)
+            break
+
+        else:
+            for varBind in varBinds:
+                 print('%s = %s' % varBind)
+
+walk('192.168.88.25', '1.3.6.1.2.1.1.9.1.2')
+get("192.168.88.25")
